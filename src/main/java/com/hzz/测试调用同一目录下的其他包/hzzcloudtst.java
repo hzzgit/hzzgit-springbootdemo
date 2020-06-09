21a7e0bfacebb22ab1a9301f3a0639b8ad0e1c64
huzz <605198347@qq.com>
2020-06-09 16:41:18 +0800

    基于freemarker生成业务代码,加入exl导出的时候序号的自动生成

---
+++ src/main/java/com/hzz/测试调用同一目录下的其他包/hzzcloudtst.java
@@ -0,0 +1,10 @@
+package com.hzz.测试调用同一目录下的其他包;
+
+import com.hzz.hzzcloud.jdbcutil.jdkjdbc.JdkDataSource;
+
+public class hzzcloudtst {
+    public static void main(String[] args) {
+        JdkDataSource.jdkmysql();
+
+    }
+}
